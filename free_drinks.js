79f0d8927c7a791a640522af1c76bb0f67f1737f
Sahidulislam05 <sahidulislamcst@gmail.com>
2025-07-28 18:30:32 +0600

    added js tasks

---
+++ free_drinks.js
@@ -0,0 +1,17 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+const burgerPrice = 200;
+let cost = 400;
+
+if (cost > 500) {
+  console.log("You get a free coke");
+}
+
+else {
+  console.log("Coke: 30 TK");
+}
