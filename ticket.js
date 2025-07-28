7c82237ea7b2397958a50ecca6fe414769f65200
Sahidulislam05 <sahidulislamcst@gmail.com>
2025-07-28 22:05:31 +0600

    added ticket problem solution

--- ticket.js
+++ ticket.js
@@ -4,21 +4,54 @@
 //     - Senior citizens (age >= 60) gets a 15% Discount
 //     - Otherwise Regular ticket fare 800 tk
 
-let age = 20;
+
+let age = 50;
 
 let isStudent = true;
 
-const price = 800;
+let price = 800;
 
 if (age < 10) {
   console.log(" You can go for free");
-} else if (isStudent) {
+}
+
+else if (isStudent && age > 10 && age <60 ) {
   price = (price * 50) / 100;
   console.log(price);
-} else if (age >= 60) {
+} 
+
+else if (age >= 60) {
   price = (price * 15) / 100;
   console.log(price);
-} else {
+} 
+
+else {
   console.log(price);
 }
 
+
+
+
+// let age = 70;
+
+// let isStudent = true;
+
+// let price = 800;
+
+// if (age < 10) {
+//   console.log(" You can go for free");
+// } 
+
+// else if (isStudent) {
+//   price = (price * 50) / 100;
+//   console.log(price);
+// } 
+
+// else if (age >= 60) {
+//   price = (price * 15) / 100;
+//   console.log(price);
+// } 
+
+// else {
+//   console.log(price);
+// }
